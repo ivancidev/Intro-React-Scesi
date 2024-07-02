0f2ae078d03fdfd42858cdc742228b80ea00c984
Herlan <herlanherbas@gmail.com>
2024-07-02 16:09:11 -0400

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css';
-import { Counter } from './Counter';
+import { Counter } from './components/Counter';
+import Tasks from './pages/Tasks/Tasks';
+import Users from './pages/UserProfile/Users';
 
 function App() {
   return (
-    <Counter />
+    // <Counter />
+    // <Tasks />
+    <Users />
   )
 }
 
