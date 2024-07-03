d55f7389b1bc590929bd96c341b48c75f1e15032
Herlan <herlanherbas@gmail.com>
2024-07-02 23:06:05 -0400

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
+import Calculator from './components/Calculator/Calculator';
 import { Counter } from './components/Counter';
+import Login from './pages/Login/Login';
 import Tasks from './pages/Tasks/Tasks';
+import UserAxios from './pages/UserProfile/UserAxios';
 import Users from './pages/UserProfile/Users';
 
 function App() {
   return (
     // <Counter />
     // <Tasks />
-    <Users />
+    // <Users />
+    // <Login />
+    // <Calculator />
+    <UserAxios />  
   )
 }
 
